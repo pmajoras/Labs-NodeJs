36727a1a9cd0b90e9630d1b30aae7dbcb677b4f1
Paulo Gabriel Souza Poffal <pgabrielpoffal@gmail.com>
2016-01-20 23:17:51 -0200

    remove unused dependencies from package.json remove unused console.log

--- src/ManagementSystemRestApi/Server/server.js
+++ src/ManagementSystemRestApi/Server/server.js
@@ -10,7 +10,6 @@ exports.start = () => {
   app.use(bodyParser());
   app.use(bodyParser.json());
   routes.setup(app, controllers);
-  console.log(process);
   var port = process.env.PORT || 8080;
   app.listen(port);
   console.log("Express server listening on port %d in %s mode", port, app.settings.env);
