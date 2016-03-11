56152ec0b546d4464705222d0c74c124a2b79d07
Paulo Gabriel Souza Poffal <pgabrielpoffal@gmail.com>
2016-03-10 21:39:45 -0300

    hotfix: add coments in model

--- src/ManagementSystemRestApi/domain/entities/task-list.js
+++ src/ManagementSystemRestApi/domain/entities/task-list.js
@@ -3,12 +3,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var taskSchema = require('task');
-
 // set up a mongoose model
 module.exports = mongoose.model('TaskList', new Schema({
   name: { type: String, required: true },
   description: { type: String, required: false },
   board: { type: Schema.Types.ObjectId, ref: 'Board' },
-  tasks: [taskSchema]
+  tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }]
 }));
--- src/ManagementSystemRestApi/domain/entities/task.js
+++ src/ManagementSystemRestApi/domain/entities/task.js
@@ -14,5 +14,6 @@ module.exports = mongoose.model('Task', new Schema({
   name: { type: String, required: true },
   description: { type: String, required: false },
   taskList: { type: Schema.Types.ObjectId, ref: 'TaskList' },
-  history: [historySchema]
+  history: [historySchema],
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
 }));
